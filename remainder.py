9b8ef6d0b6c0edc3b96ff86637358739b51f1333
worknech <worknech@yandex.ru>
2025-07-24 21:44:10 +0300

    Добавлена функция установки времени напоминания

--- remainder.py
+++ remainder.py
@@ -8,7 +8,18 @@ import pygame
 
 
 def set():
-    pass
+    rem = sd.askstring('Время напоминания', 'Введите время напоминания ыв формате ЧЧ:ММ (в 24 часовом формате)')
+    try:
+        hour = int(rem.split(':')[0])
+        minute = int(rem.split(':')[1])
+        now = datetime.datetime.now()
+        print(now)
+        dt = now.replace(hour=hour, minute=minute)
+        print(dt)
+        t = dt.timestamp()
+        print(t)
+    except Exception as e:
+        mb.showerror('Ошибка!', f'Произошла ошибка {e}')
 
 
 root = Tk()
