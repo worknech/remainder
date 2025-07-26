e5b934e8d74cd8aa789763f0df20ba4ae035223c
worknech <worknech@yandex.ru>
2025-07-26 21:18:54 +0300

    Добавлено отображение текста напоминания

--- remainder.py
+++ remainder.py
@@ -22,7 +22,8 @@ def set_t():
         print(dt)
         t = dt.timestamp()
         print(t)
-        label.config(text=f'Напоминание на {hour:02}:{minute:02}')
+        text = sd.askstring('Текст напоминания', 'Введите текст напоминания')
+        label.config(text=f'Напоминание на {hour:02}:{minute:02} с текстом "{text}"')
     except Exception as e:
         mb.showerror('Ошибка!', f'Произошла ошибка {e}')
 
